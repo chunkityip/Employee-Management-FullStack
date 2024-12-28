141e0119d68444e9839313c967648d1e3b0ee992
chunkityip <alex0917lfo@hotmail.com>
2024-12-28 06:44:54 -0500

    test liquibase

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,10 +24,10 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.liquibase:liquibase-core")
 	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
