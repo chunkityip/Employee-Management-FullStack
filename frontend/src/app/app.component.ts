56d62b6058a3affa8e6a3cc5585f97e68ba57a8b
chunkityip <alex0917lfo@hotmail.com>
2025-01-19 14:57:55 -0500

    implement all components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// Angular Data Grid 
+import { AgGridModule } from 'ag-grid-angular';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth/auth.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+  isLogin: Observable<boolean>;
+  key: any;
+  title = 'Employee-registration-client';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isLogin = this.authService.isLoggedIn;
+  }
+  
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { DeleteComponent } from './delete/delete.component';
+import { EmployeeprofileComponent } from './employeeprofile/employeeprofile.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SearchComponent } from './search/search.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent,
+    DeleteComponent,
+    EmployeeprofileComponent,
+    HeaderComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegistrationComponent,
+    SearchComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
